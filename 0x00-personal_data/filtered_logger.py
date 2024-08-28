5262b600efbfd534ac48681b73139e6721d77c83
mikelexx <michael.muri2020@gmail.com>
2024-08-28 23:29:56 +0300

    0. Regex-ing

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+obfuscating sensitive data
+"""
+import re
+from typing import List
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """
+    obfuscates a log message (message)
+    Args:
+        fields: a list of strings representing all fields to obfuscate
+        redaction: a string representing by what the field will be obfuscated
+        message: a string representing the log line
+        separator: a string representing by which character is separating
+        all fields in
+        the log line (message)
+    returns: the log message obfuscated
+    """
+    return re.sub(
+        r'([^;]*=[^;]*);',
+        lambda match: re.sub(r'=.+', f'={redaction}{separator}',
+                                     match.group(0))
+        if match.group(0).split('=')[0] in fields else match.group(0),
+        message
+    )
