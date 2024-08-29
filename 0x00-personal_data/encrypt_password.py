06867652650d22d2414ddd419cfd8826e5c68056
mikelexx <michael.muri2020@gmail.com>
2024-08-29 18:33:40 +0300

    simplify code

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -3,13 +3,11 @@
 Encrypting passwords, user passwords should never be stored
 in plain text in database
 """
-from typing import ByteString
 import bcrypt
 
 
-def hash_password(password: str) -> ByteString:
+def hash_password(password: str) -> bytes:
     """
     returns a salted, hashed password, which is a byte string.
     """
-    hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-    return hashed
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
