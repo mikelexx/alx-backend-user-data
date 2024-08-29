ebc09b98d58ef15a5766bf1ab3fcdf1fff747da6
mikelexx <michael.muri2020@gmail.com>
2024-08-29 18:14:27 +0300

    5. Encrypting passwords

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Encrypting passwords, user passwords should never be stored
+in plain text in database
+"""
+from typing import ByteString
+import bcrypt
+
+
+def hash_password(password: str) -> ByteString:
+    """
+    returns a salted, hashed password, which is a byte string.
+    """
+    hashed = bcrypt.hashpw(bytes(password, 'utf-8'), bcrypt.gensalt())
+    return hashed
