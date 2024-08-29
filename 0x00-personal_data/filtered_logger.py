e5a970ebd5899c9fb46015d2e0ecb9035cc6a162
mikelexx <michael.muri2020@gmail.com>
2024-08-29 08:01:54 +0300

    1. Log Formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,7 +3,29 @@
 obfuscating sensitive data
 """
 import re
-from typing import List
+from typing import List, Optional, Union
+
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: Optional[List[str]] = None):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = [] if fields is None else fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        formats a log record while obfuscating sensitive fields
+        """
+        return filter_datum(self.fields, self.REDACTION, record.getMessage(),
+                            self.SEPARATOR)
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
