50a3d8681129d0fa574146afc146f9aedbd9c380
mikelexx <michael.muri2020@gmail.com>
2024-09-02 19:41:07 +0300

    duplicate SimpleApi to __MACOSX

---
+++ 0x01-Basic_authentication/__MACOSX/SimpleAPI/api/v1/auth/auth.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+authentication system
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """
+    for managing API authentication
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        checks if the request path is excluded
+        """
+        if path is None or not excluded_paths:
+            return True
+        if path in excluded_paths or path + '/' in excluded_paths:
+            return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """
+        checks for authorization_header in request and returns one
+        if found
+        """
+        if not request or not request.headers.get('Authorization'):
+            return None
+        return request.headers.get('Authorization')
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        TODO -> GET CURRENT USER OF THE REQUEST FROM DATABASE
+        """
+        return None
