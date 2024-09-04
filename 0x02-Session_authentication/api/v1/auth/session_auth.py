675174c6f672f4dbe4a1d23360bef89e7d0c3be1
mikelexx <michael.muri2020@gmail.com>
2024-09-04 12:18:27 +0300

    Remove .pyc files from tracking

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+implementing session authentication system
+"""
+from .auth import Auth
+
+
+class SessionAuth(Auth):
+    """
+    system that uses session authentication mechanisms
+    """
+    pass
