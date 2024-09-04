fe337dc950fafa6eaa749d5cb627e4ee03375474
mikelexx <michael.muri2020@gmail.com>
2024-09-04 13:06:58 +0300

    4. Session cookie

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -2,6 +2,7 @@
 """
 authentication system
 """
+import os
 from flask import request
 from typing import List, TypeVar
 
@@ -10,6 +11,7 @@ class Auth:
     """
     for managing API authentication
     """
+    _my_session_id = os.getenv('SESSION_NAME')
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
@@ -38,3 +40,12 @@ class Auth:
         TODO -> GET CURRENT USER OF THE REQUEST FROM DATABASE
         """
         return None
+
+    def session_cookie(self, request=None):
+        """
+        returns a cookie value(session id) from a request
+        """
+        if not request:
+            return
+        session_id = request.cookies.get(self._my_session_id)
+        return session_id
