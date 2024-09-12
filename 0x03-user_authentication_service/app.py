08d6875dab9ed8e75427621e5c8c024aeff1b859
mikelexx <michael.muri2020@gmail.com>
2024-09-12 12:03:06 +0300

    capitalize methods of flask route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -79,7 +79,7 @@ def profile() -> str:
     return jsonify({'email': user.email})
 
 
-@app.route('/reset_password', methods=['post'], strict_slashes=False)
+@app.route('/reset_password', methods=['POST'], strict_slashes=False)
 def get_reset_password_token() -> str:
     """
     generates a token
