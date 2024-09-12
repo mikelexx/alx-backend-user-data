a2408ac53efe9c63672ecc52afa3e8265854d1fd
mikelexx <michael.muri2020@gmail.com>
2024-09-12 11:16:23 +0300

    abort with 401 if not authorized

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -43,7 +43,7 @@ def login():
     email = request.form.get('email')
     password = request.form.get('password')
     if not auth.valid_login(email, password):
-        abort(404)
+        abort(401)
     session_id = auth.create_session(email)
     response = jsonify({"email": email, "message": "logged in"})
     response.set_cookie('session_id', session_id)
